879d76830ad0adc018b16e8750779abf5881f9b2
evan.lightcap <evan.lightcap@cloudkitchens.com>
2023-06-14 13:59:47 -0400

    fix timezones

--- main.py
+++ main.py
@@ -27,7 +27,8 @@ def scrape_and_post_tweet():
 
     if tweets:
         json_object = json.loads(tweets)
-        tweet_id = list(json_object.keys())[2]
+        #get the id of the second tweet bc they have one pinned. thisll change if they unpin it
+        tweet_id = list(json_object.keys())[1]
 
         # Check if it's a new tweet
         if tweet_id != latest_tweet_id:
@@ -40,25 +41,30 @@ def scrape_and_post_tweet():
                 print("too old")
                 return
             latest_tweet_id = tweet_id
+            #get content of tweet and check if it contains spawn.
             tweet_content = json_object[tweet_id]['content']
             if("spawn" not in tweet_content):
                 print("dont care")
                 return
-            # Post the tweet content to Discord
+            # spit the tweet into individual words. use this to get the boss name and spawn time. thinking about location
             split = tweet_content.split(' ')
             if split[1]!="will" and "spawn" not in split[1]:
                 boss_name = f"{split[0]} {split[1]}"
             else:
                 boss_name = split[0]
+            #gotta prepend a 0 to the time to make it 24 hour bc
             if len(split[-3]) < 6:
                 shittytime = split[-3].strip("(")
                 split[-3] = ("0{}".format(shittytime))
+            #split[-3] is the hours and minutes, split[-2] is AM or PM. theres a parenthesis in the way, so we gotta strip those out.
             spawntime = (f"{split[-3]}:00 {split[-2]}").strip("(").strip(")")
             spawntimelong = timeconvert(spawntime)
             spawntimesplit = spawntimelong.split(":")
+            #to unix timestamp. its split at "." to strip milliseconds out.
             spawntimeobject = datetime(now_est.year,now_est.month,now_est.day,int(spawntimesplit[0]),int(spawntimesplit[1]),int(spawntimesplit[2]))
+            spawngmt = spawntimeobject + timedelta(hours=4)
             unixtime = str((time.mktime(spawntimeobject.timetuple()))).split(".")[0]
-
+            #send the discord message
             embed = DiscordEmbed(title='World Boss Spawning!', color='03b2f8')
             embed.set_thumbnail(url=json_object[tweet_id]['images'][0])
             embed.set_image(url=json_object[tweet_id]['images'][1])
