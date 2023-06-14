a9e889d6766c1d6d1d796c0b392fae8a8009eb9f
evan.lightcap <evan.lightcap@cloudkitchens.com>
2023-06-13 21:09:12 -0400

    oooooo datetime nonsense

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ def scrape_and_post_tweet():
         # Check if it's a new tweet
         if tweet_id != latest_tweet_id:
             now = datetime.now(timezone.utc)
+            now_est = datetime.now()
             then = now + timedelta(minutes=-5)
             post_time = json_object[tweet_id]['posted_time']
             post_dto = parser.parse(post_time)
@@ -49,12 +50,20 @@ def scrape_and_post_tweet():
                 boss_name = f"{split[0]} {split[1]}"
             else:
                 boss_name = split[0]
-            spawntime = (f"{split[-3]} {split[-2]} {split[-1]}").strip("(")
+            if len(split[-3]) < 2:
+                shittytime = split[-3].strip("(")
+                split[-3] = ("0{}".format(time))
+            spawntime = (f"{split[-3]}:00 {split[-2]}").strip("(").strip(")")
+            spawntimelong = timeconvert(spawntime)
+            spawntimesplit = spawntimelong.split(":")
+            spawntimeobject = datetime(now_est.year,now_est.month,now_est.day,int(spawntimesplit[0]),int(spawntimesplit[1]),int(spawntimesplit[2]))
+            unixtime = str((time.mktime(spawntimeobject.timetuple()))).split(".")[0]
+
             embed = DiscordEmbed(title='World Boss Spawning!', color='03b2f8')
             embed.set_thumbnail(url=json_object[tweet_id]['images'][0])
             embed.set_image(url=json_object[tweet_id]['images'][1])
             embed.add_embed_field(name="Boss", value=boss_name)
-            embed.add_embed_field(name="Spawn Time", value=spawntime)
+            embed.add_embed_field(name="Spawn Countdown", value=f"<t:{unixtime}:R>")
             embed.set_footer(text="https://github.com/elightcap/d4bot")
             embed.set_timestamp()
             webhook = DiscordWebhook(url=discord_webhook_url, content="@here")
@@ -65,6 +74,16 @@ def scrape_and_post_tweet():
             print("No new tweets.")
     else:
         print("No tweets found for the specified user.")
+
+def timeconvert(str1):
+    if str1[-2:] == "AM" and str1[:2] == "12":
+        return "00" + str1[2:-2]
+    elif str1[-2:] == "AM":
+        return str1[:-2]
+    elif str1[-2:] == "PM" and str1[:2] == "12":
+        return str1[:-2]
+    else:
+        return str(int(str1[:2]) + 12) + str1[2:8]
 
 # Schedule the job to run every minute
 schedule.every(5).minutes.do(scrape_and_post_tweet)
